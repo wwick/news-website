ac914b862b0d97fdff2c278fefa4c0bdcb84a42c
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-16 16:11:05 -0600

    stories

--- homepage.php
+++ homepage.php
@@ -1,47 +1,47 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>News</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>News</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
-    <table>
-        <tr>
-            <th>Title</th>
-            <th>Author</th>
-        </tr>  
+<table>
+<tr>
+<th>Title</th>
+<th>Author</th>
+</tr>  
 
 <?php
 
-    require 'database.php';
+require 'database.php';
+session_start();
+$stmt = $mysqli->prepare("select title, author, id from stories");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
 
-    $stmt = $mysqli->prepare("select title, author, id from stories");
-    if(!$stmt){
-        printf("Query Prep Failed: %s\n", $mysqli->error);
-        exit;
-    }
+$stmt->execute();
 
-    $stmt->execute();
+$stmt->bind_result($tile, $author, $story_id);
 
-    $stmt->bind_result($tile, $author, $story_id);
-
-    while($stmt->fetch()){
-        echo "<tr>\n";
-        printf("\t<td> <a href=\"story.php?id=$story_id\">%s</a></td>\n", htmlspecialchars($tile));
-        printf("\t<td>%s</td>\n", htmlspecialchars($author));
-        echo "</tr>\n";
-    }
-    echo "</tr>\n";
-    $stmt->close();
+while($stmt->fetch()){
+	echo "<tr>\n";
+	printf("\t<td> <a href=\"story.php?id=$story_id\">%s</a></td>\n", htmlspecialchars($tile));
+	printf("\t<td>%s</td>\n", htmlspecialchars($author));
+	echo "</tr>\n";
+}
+echo "</tr>\n";
+$stmt->close();
 
 ?>
 
 </table>
 <a href="write.php">Click here to write a story</a>
 
-        
+
 
 
 
