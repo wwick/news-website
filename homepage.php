021c5f12b99f220bcb8b7e17ebe85e03cf2b36bd
wwick <wwick@wustl.edu>
2019-02-16 16:22:16 -0600

    fixed merge conflicts

--- homepage.php
+++ homepage.php
@@ -35,17 +35,7 @@ while($stmt->fetch()){
 }
 echo "</tr>\n";
 $stmt->close();
-    $stmt->bind_result($tile, $author, $story_id);
-
-    while($stmt->fetch()){
-        echo "<tr>\n";
-        printf("\t<td> <a href=\"story.php?id=$story_id\">%s</a></td>\n", htmlspecialchars($tile));
-        printf("\t<td>%s</td>\n", htmlspecialchars($author));
-        echo "</tr>\n";
-    }
-    echo "</tr>\n";
-    $stmt->close();
-    $mysqli->close();
+$mysqli->close();
 
 ?>
 
