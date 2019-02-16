0b1a92dc59acaa2a0816a804aa81b2d35f8c7af7
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-16 14:51:29 -0600

    Story writing ability has been added

--- upload.php
+++ upload.php
@@ -1,4 +1,22 @@
 <?php
 require "database.php";
 session_start();
-$userID = 
+$userID =  $_SESSION["userid"];
+$story = $_POST["story"];
+$author = $_POST["author"];
+$title = $_POST["title"];
+
+
+$stmt = $mysqli->prepare("insert into stories (story, title, author) values (?,?,?)");
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt->bind_param('sss', $story, $title, $author);
+
+$stmt->execute();
+
+$stmt->close();
+header("Location:homepage.php");
+?>
