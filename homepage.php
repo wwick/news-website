ec15413f4bd3f39effba45fecf4154fcc88b0a87
wwick <wwick@wustl.edu>
2019-02-16 12:38:02 -0600

    homepage

---
+++ homepage.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>News</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Author</th>
+        </tr>  
+
+<?php
+
+    require 'database.php';
+
+    $stmt = $mysqli->prepare("select title, author from stories");
+    if(!$stmt){
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
+    }
+
+    $stmt->execute();
+
+    $stmt->bind_result($tile, $author);
+
+    echo "<tr>\n";
+    while($stmt->fetch()){
+        printf("\t<td>%s</td>\n", htmlspecialchars($tile));
+        printf("\t<td>%s</td>\n", htmlspecialchars($author));
+    }
+    echo "</tr>\n";
+    $stmt->close();
+
+?>
+
+</table>
+
+
+        
+
+
+
+
+
+
+
+</body>
+</html>
