c48f765aa864a4210158c373ea7cd78e5adc2b83
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-16 12:58:49 -0600

    moving locations

--- login.php
+++ login.php
@@ -5,7 +5,23 @@ $user = $_POST["user"];
 $_SESSION["user"] = $user;
 $password = $_POST["password"];
 $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+$taken = 0;
 if($_POST["new"]){
+	$stmt = $mysqli->prepare("select id, user, password from users");
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+	$stmt->execute();
+	$stmt->bind_result($id, $tableUser, $tablePass);
+	while($stmt->fetch()){
+		if($user === $tableUser){
+			$taken = 1;
+			break;        
+		}
+
+	}if
+	$stmt->close;
 	$stmt = $mysqli->prepare("insert into users (user, password) values ('$user', '$hashedPass')");
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -13,6 +29,7 @@ if($_POST["new"]){
 	}
 	$stmt->execute();
 	$stmt->close();
+	//header("Location:homepage.php");
 
 } else {
 	$stmt = $mysqli->prepare("select id, user, password from users");
@@ -25,7 +42,7 @@ if($_POST["new"]){
 	while($stmt->fetch()){
 		if($user === $tableUser){
 			if(password_verify($password, $tablePass)){
-				header("Location:login.html");
+				header("Location:homepage.php");
 			} else{
 				echo "wrong password";
 				break;
