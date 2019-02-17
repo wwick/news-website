3e310627ef3dcf48be7e51bc514342fe71f8eab0
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-17 12:04:30 -0600

    Updating stories now works

--- update.php
+++ update.php
@@ -2,7 +2,6 @@
 require "database.php";
 session_start();
 if(isset($_GET['cid'])){
-	echo "cid is set";
 	$stmt = $mysqli->prepare("select user_id from comments where comment_id=".$_GET["cid"]);
 	if(!$stmt){
 		printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -24,5 +23,28 @@ if(isset($_GET['cid'])){
 		$stmt->close();
 		header("Location:story.php?id={$_GET['sid']}");
 	}	
+} else{
+	$stmt = $mysqli->prepare("select user_id from stories where story_id=".$_GET["sid"]);
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+	echo "passed first";
+	$stmt->execute();
+	$stmt->bind_result($userID);
+	while($stmt->fetch()){
+		$id = $userID;
+	}
+	if($_SESSION["user"]==$id){
+		$stmt = $mysqli->prepare('update stories set story=? where story_id='.$_GET['sid']);
+		if(!$stmt){
+			printf("Query Prep Failed: %s\n", $mysqli->error);
+			exit;
+		}
+		$stmt->bind_param('s',$_POST['story']);
+		$stmt->execute();
+		$stmt->close();
+		header("Location:story.php?id={$_GET['sid']}");
+	}
 }
 ?>
