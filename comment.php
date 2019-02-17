a9f791b4cff4383f664b65052416ef6bf448d01c
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-16 18:02:34 -0600

    commenting works

--- comment.php
+++ comment.php
@@ -2,14 +2,17 @@
 require "database.php";
 session_start();
 $comment = $_POST["comment"];
-$user = $_SESSION["user_id"];
-$storyID = $_SESSION["storyID"];
+$user = $_SESSION["user"];
+$storyID = $_SESSION["story"];
+echo $user;
+echo $storyID;
+echo $comment;
 $stmt = $mysqli->prepare("insert into comments (story_id, user_id, comment) values (?, ?, ?)");
 if(!stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
 }
-$stmt->bind_param('iis', $user, $storyID, $comment);
+$stmt->bind_param('iis', $storyID, $user, $comment);
 $stmt->execute();
 $stmt->close();
 ?>
--- story.php
+++ story.php
@@ -1,44 +1,45 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Story</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Story</title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
 
 </head>
 <body>
 
-    <?php
+<?php
 
-    require 'database.php';
-    $story_id=$_REQUEST['id'];
+require 'database.php';
+$story_id=$_REQUEST['id'];
+session_start();
+$_SESSION["story"] = $story_id;
+$stmt = $mysqli->prepare("select title, author, story from stories where story_id=".$story_id);
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
 
-    $stmt = $mysqli->prepare("select title, author, story from stories where story_id=".$story_id);
-    if(!$stmt){
-        printf("Query Prep Failed: %s\n", $mysqli->error);
-        exit;
-    }
+$stmt->execute();
 
-    $stmt->execute();
+$stmt->bind_result($title, $author, $story);
 
-    $stmt->bind_result($title, $author, $story);
+while($stmt->fetch()){
+	printf("\t<p>%s</p>\n", htmlspecialchars($title));
+	printf("\t<p>%s<p>\n", htmlspecialchars($author));
+	printf("\t<p>%s</p>\n", htmlspecialchars($story));
 
-    while($stmt->fetch()){
-        printf("\t<p>%s</p>\n", htmlspecialchars($title));
-        printf("\t<p>%s<p>\n", htmlspecialchars($author));
-        printf("\t<p>%s</p>\n", htmlspecialchars($story));
+}
+$stmt->close();
+$mysqli->close();
 
-    }
-    $stmt->close();
-    $mysqli->close();
-
-    ?>
+?>
 <h3>Comment here!</h3></br>
-<textarea placeholder="Type your comment here!" name="comment" form="comment"></textarea></br>
-<form action="comment.php" method="post">
+<form action="comment.php" method="post" id="comment">
 <input type="submit" value="Comment">
 </form>
+<textarea placeholder="Type your comment here!" name="comment" form="comment"></textarea></br>
 </body>
 </html>
 
