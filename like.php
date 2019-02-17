6fe34c28a9e718ab53c3b40a955f97a0b1790d27
wwick <wwick@wustl.edu>
2019-02-17 14:33:10 -0600

    trying to get like button to work

--- like.php
+++ like.php
@@ -1,14 +1,17 @@
 <?php
 
 require 'database.php';
-$story_id=$_REQUEST['id'];
 session_start();
 if (!(isset($_SESSION['story']))) {
-    header("Location:homepage.php");
+    //header("Location:homepage.php");
+    echo "story_id session variable not set";
+    exit;
 }
 $story_id = $_SESSION['story'];
 if (!(isset($_SESSION['user']))) {
-    header("Location:story.php?=".$story_id);
+    //header("Location:story.php?=".$story_id);
+    echo "user_id session variable not set";
+    exit;
 }
 $user_id = $_SESSION['user'];
 
@@ -27,14 +30,18 @@ $already_liked = false;
 
 if ($stmt->fetch()){
     $liked_array = explode(",", $liked_stories);
-    $already_liked = in_array($story_id,$liked_array);
+    $already_liked = in_array((string)$story_id,$liked_array);
 }
 
 $stmt->close();
 
 if ($already_liked) {
-    header("Location:story.php?=".$story_id);
+   //header("Location:story.php?=".$story_id);
+   echo "already liked";
+	exit;
 }
+
+echo "continued past already liked if statement";
 
 $stmt = $mysqli->prepare("UPDATE stories SET likes=likes+1 WHERE story_id=".$story_id);
 if(!$stmt){
@@ -44,7 +51,7 @@ if(!$stmt){
 $stmt->execute();
 $stmt->close();
 
-$stmt = $mysqli->prepare("UPDATE users SET liked_stories=(liked_stories + \",\" + ".$story_id.") WHERE user_id=".$user_id);
+$stmt = $mysqli->prepare("UPDATE users SET liked_stories=concat(liked_stories, \",\", ".$story_id.") WHERE user_id=".$user_id);
 if(!$stmt){
 	printf("Query Prep Failed: %s\n", $mysqli->error);
 	exit;
@@ -54,6 +61,7 @@ $stmt->close();
 
 $mysqli->close();
 
-header("Location:story.php?=".$story_id);
+//header("Location:story.php?=".$story_id);
+echo "reached end";
 
 ?>
