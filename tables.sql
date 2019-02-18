00596d9d15b61743c1794d1880e7b5f281d0c7e7
Will Wick <wwick@wustl.edu>
2019-02-18 17:12:28 +0000

    tables.sql edited online with Bitbucket

--- tables.sql
+++ tables.sql
@@ -2,33 +2,31 @@ CREATE TABLE `users` (
   `user_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `liked_stories` varchar(100) NOT NULL DEFAULT '0',
+  `liked_stories` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user` (`user`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `stories` (
   `story_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` smallint(5) unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
-  `story` text NOT NULL,
+  `story` longtext NOT NULL,
   `author` varchar(100) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `likes` smallint(6) NOT NULL DEFAULT '0',
+  `likes` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`story_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `stories_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `comments` (
   `comment_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `story_id` smallint(5) unsigned NOT NULL,
   `user_id` smallint(5) unsigned NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `comment` text NOT NULL,
   PRIMARY KEY (`comment_id`),
   KEY `story_id` (`story_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`story_id`) REFERENCES `stories` (`story_id`),
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
